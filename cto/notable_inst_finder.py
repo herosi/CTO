c1faa413d6ecf3c9f137ac47eb49f7d3ae4ca406
Hiroshi Suzuki <20717881+herosi@users.noreply.github.com>
2024-12-19 20:04:49 +0900

    - Added std::basic_string patterns - Added multiple instruction search with quick hack on instruction   finder

--- cto/notable_inst_finder.py
+++ cto/notable_inst_finder.py
@@ -9,20 +9,19 @@ import ida_segment
 
 import os
 import json
+import re
 
 class notable_inst_t(object):
     def __init__(self, frules=os.path.join(os.path.dirname(__file__),"notable_insts.json"), show_high_ent=False, cmt_prefix="NT_INST: "):
         with open(frules) as f:
             insts = json.load(f)
         self.insts = {}
+        self.minsts = {}
         for rule_type in insts:
             if rule_type not in self.insts:
                 self.insts[rule_type] = {}
-            if rule_type == "single":
-                for rule_name in insts[rule_type]:
-                    self.insts[rule_type][rule_name] = insts[rule_type][rule_name]
-            else:
-                pass
+            for rule_name in insts[rule_type]:
+                self.insts[rule_type][rule_name] = insts[rule_type][rule_name]
         #print(self.insts)
         self.cmt_prefix = cmt_prefix
         
@@ -38,12 +37,39 @@ class notable_inst_t(object):
             ea = ida_search.find_text(ea, 0, 0, rule, flags)
             
     def get_notable_insts(self):
-        multi_rule_result = {}
         for rule_name in self.insts["single"]:
             rule = self.insts["single"][rule_name]["rule"]
             regex = self.insts["single"][rule_name]["regex"]
             for ea in self.get_inst_eas(rule, regex=regex):
                 yield ea, rule_name
+                
+        flags = ida_search.SEARCH_DOWN|ida_search.SEARCH_NEXT
+        for rule_name in self.insts["multiple"]:
+            rules = self.insts["multiple"][rule_name]["rules"]
+            regex = self.insts["multiple"][rule_name]["regex"]
+            for ea in self.get_inst_eas(rules[0][0], regex=regex):
+                if ea != ida_idaapi.BADADDR:
+                    first_ea = ea
+                    found_flag = True
+                    for rule, tolerance in rules[1:]:
+                        false_times = 0
+                        while false_times < tolerance and ea != ida_idaapi.BADADDR:
+                            ea = ida_search.find_code(ea, flags)
+                            disasm = idc.generate_disasm_line(ea, 0)
+                            #print(hex(ea), disasm)
+                            if not re.match(rule, disasm):
+                                false_times += 1
+                                found_flag = False
+                            elif ea == ida_idaapi.BADADDR:
+                                found_flag = False
+                                break
+                            else:
+                                found_flag = True
+                                break
+                        if not found_flag:
+                            break
+                    if found_flag:
+                        yield first_ea, rule_name
     
     def set_cmt(self, ea, rule_name):
         cmt = ida_bytes.get_cmt(ea, False)
